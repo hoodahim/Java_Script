410a26bed9e01a85bb02e6a56377e2edda72874f
himhooda <himanshu243639@gmail.com>
2022-12-04 13:12:46 +0200

    Module 3 (exercise3)

--- Module3/module3-starters-main/module3-starters-main/t3/3.js
+++ Module3/module3-starters-main/module3-starters-main/t3/3.js
@@ -1,2 +1,13 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+console.log(names.length);
+console.log(names[1]);
+
+const name = document.getElementById('target');
+
+for ( let i = 0; i<names.length; i++){
+  let item = document.createElement('li');
+  name.appendChild(item);
+  document.getElementsByTagName('li')[i].innerText = names[i];
+}
